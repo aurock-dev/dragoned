624b0622a3fa1f362b863e039e9e5e6d90f0aa6a
aurock <jeremy.dussol@epsi.fr>
2022-10-30 17:30:34 +0100

    💄 style option and server view

--- client/interactions.js
+++ client/interactions.js
@@ -1,16 +1,10 @@
 //#region -- DIALOG CHANGE NAME
 let dialogChangeName = document.querySelector('#dialogNameChange');  
 
-$(document).on('click', '#showDialogChangeName', function(){
-    dialogChangeName.show();
-})
 $(document).on('click', '#validateChangeName', function(){
     let playerName = document.querySelector('#inputChangeName').value;
     setLSPlayerName(playerName)
     socket.emit('updateClientName', getLSPlayerName());
-    dialogChangeName.close();
-})
-$(document).on('click', '#cancelChangeName', function(){
     dialogChangeName.close();
 })
 //#endregion
