44b4abaa0fd8bc9af700ff6304829adfc1d9b786
aurock <jeremy.dussol@epsi.fr>
2022-11-06 16:11:25 +0100

    📝 add doc for localStorageManager

--- client/localStorageManager.js
+++ client/localStorageManager.js
@@ -1,3 +1,7 @@
+/**
+ * On first player connection, ask for a name and save player object to local storage
+ *
+ */
 function firstConnection(){
     dialogChangeName.show();
     $(document).on('click', '#validateFirstName', function(){
@@ -8,14 +12,28 @@ function firstConnection(){
     })
 }
 
+/**
+ * Store player object in local storage
+ *
+ * @param {*} player object
+ */
 function setLSPlayer(player){
     localStorage.setItem('player', JSON.stringify(player));
 }
 
+/**
+ * Return player object from local storage
+ *
+ * 
+ */
 function getLSPlayer(){
     return JSON.parse(localStorage.getItem('player'));
 }
 
+/**
+ * Clear local storage in needed
+ *
+ */
 function resetLocalStorage(){
     localStorage.clear();
 }
