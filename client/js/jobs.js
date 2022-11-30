8beb91c8d9a89194bd3ba77b1b09d2a378ed3f01
jeremy.dussol <jdussol@ct2mc.com>
2022-11-30 13:05:54 +0100

    🐛 save player when loot

--- client/js/jobs.js
+++ client/js/jobs.js
@@ -1,21 +1,19 @@
 $(document).on('click', '#woodcutting', () => {
     var percent = 0;
     if (player.states.jobbing === null){
-        console.log('wood start')
         player.states.jobbing = setInterval(() => {
             percent += 50;
             $('.woodcuttingTime').width(calcPercentage(percent, player.job.woodcutting.time)+'%');
             document.querySelector('#currentTimeWoodcutting').textContent = ((player.job.woodcutting.time - percent)/1000).toFixed(0) +' sec'
             if (percent >= player.job.woodcutting.time){
-                console.log('looting')
                 calcWoodcuttingLoot();
+                setLSPlayer(player);
                 updatePlayerRessourcesInfos();
                 percent = 0
             }
         }, 50);
     }
     else{
-        console.log('job stop')
         clearInterval(player.states.jobbing);
         player.states.jobbing = null;
         $('.woodcuttingTime').width('0%');
@@ -28,21 +26,19 @@ $(document).on('click', '#woodcutting', () => {
 $(document).on('click', '#mining', () => {
     var percent = 0;
     if (player.states.jobbing === null){
-        console.log('mine start')
         player.states.jobbing = setInterval(() => {
             percent += 50;
             $('.miningTime').width(calcPercentage(percent, player.job.mining.time)+'%');
             document.querySelector('#currentTimeMining').textContent = ((player.job.mining.time - percent)/1000).toFixed(0) +' sec'
             if (percent >= player.job.mining.time){
-                console.log('looting')
                 calcMiningLoot();
+                setLSPlayer(player);
                 updatePlayerRessourcesInfos();
                 percent = 0
             }
         }, 50);
     }
     else{
-        console.log('job stop')
         clearInterval(player.states.jobbing);
         player.states.jobbing = null;
         $('.woodcuttingTime').width('0%');
